3710d5e91b49bf77ab3756acc30bc067bb8dafba
kyleturco <kyleturco@gmail.com>
2020-11-11 15:43:21 -0600

    add basic ant design setup

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <div id="app">
     <BasicCard title='My Test Vue App' />
+    <a-button type='primary'>Test</a-button>
   </div>
 </template>
 
 <script>
-import BasicCard from './components/BasicCard.vue';
+import { Button } from 'ant-design-vue'
+import BasicCard from './components/BasicCard.vue'
 
 export default {
   name: 'App',
   components: {
     BasicCard,
+    'a-button': Button,
   },
-};
+}
 </script>
 
 <style>
+@import '~ant-design-vue/dist/antd.css';
 @import 'sass/app.scss';
 </style>
